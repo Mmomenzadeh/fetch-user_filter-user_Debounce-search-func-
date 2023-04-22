47e1363198fedf688c55b71f7b97ea0f8a15c4e9
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-04-22 23:40:39 +0430

    Debounce the search function

--- js/script.js
+++ js/script.js
@@ -55,15 +55,17 @@ async function ShowUser(userData = null) {
 
 (async function () {
   const userData = await ShowUser();
-
+  let timeOut;
   document.querySelector("#filter").addEventListener("keyup", (e) => {
     const queryString = e.target.value.toLowerCase();
+    clearTimeout(timeOut);
+    timeOut = setTimeout(() => {
+      const filteredUser = userData.filter((item) => {
+        const name = `${item.name.title} ${item.name.first} ${item.name.last}`;
+        return name.toLowerCase().indexOf(queryString) > -1;
+      });
 
-    const filteredUser = userData.filter((item) => {
-      const name = `${item.name.title} ${item.name.first} ${item.name.last}`;
-      return name.toLowerCase().indexOf(queryString) > -1;
-    });
-
-    ShowUser(filteredUser);
+      ShowUser(filteredUser);
+    }, 500);
   });
 })();
